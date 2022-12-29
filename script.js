31ceb0aaa57a55df5499fcb97f0c2056f64fef43
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2022-12-29 20:31:35 +0200

    Opening and closing modal

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const onShowModal = () => {
+  console.log('brtbbbbbbbbb');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const onCloseModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+showModalBtns.forEach(btn => btn.addEventListener('click', onShowModal));
+closeModalBtn.addEventListener('click', onCloseModal);
+overlay.addEventListener('click', onCloseModal);
